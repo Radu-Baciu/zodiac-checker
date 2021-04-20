76b05f27698d9936003371c4dafd9a9e3b43c2b3
Radu-Baciu <81168417+Radu-Baciu@users.noreply.github.com>
2021-04-20 13:43:18 +0300

    Testing autumn zodiac service.

--- Client/src/main/java/Main.java
+++ Client/src/main/java/Main.java
@@ -3,4 +3,13 @@ import io.grpc.ManagedChannelBuilder;
 import proto.*;
 
 public class Main {
+    public static void main(String[] args) {
+        ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost",8999).usePlaintext().build();
+
+        AutumnCheckerGrpc.AutumnCheckerBlockingStub autumnStub = AutumnCheckerGrpc.newBlockingStub(channel);
+        String date = "09/25/1999";
+        ZodiacChecker.ZodiacReply reply = autumnStub.giveInfoAutumn(ZodiacChecker.ZodiacRequest.newBuilder().setDate(date).build());
+        System.out.println("Server replied with " + reply);
+        channel.shutdown();
+    }
 }
--- Server/src/main/java/services/AutumnCheckerImpl.java
+++ Server/src/main/java/services/AutumnCheckerImpl.java
@@ -4,19 +4,19 @@ import io.grpc.stub.StreamObserver;
 import proto.AutumnCheckerGrpc;
 import proto.ZodiacChecker;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.util.Scanner;
 
 public class AutumnCheckerImpl extends AutumnCheckerGrpc.AutumnCheckerImplBase {
     @Override
-    public void giveInfoAutumn (ZodiacChecker.ZodiacRequest request, StreamObserver<ZodiacChecker.ZodiacReply> response) {
+    public void giveInfoAutumn (ZodiacChecker.ZodiacRequest request, StreamObserver<ZodiacChecker.ZodiacReply> responseObserver) {
         String[][] zodiac = new String[3][3];
         int zodiacBuilder = 0;
-        File file = new File ("AutumnZodiac.txt");
-
         try {
-            Scanner fileReader = new Scanner(file);
+            Scanner fileReader = new Scanner(new BufferedReader(new FileReader("src/main/java/services/AutumnZodiac.txt")));
             while (fileReader.hasNext()) {
                 String input = fileReader.next();
                 zodiac[zodiacBuilder/3][zodiacBuilder%3] = input;
@@ -30,14 +30,16 @@ public class AutumnCheckerImpl extends AutumnCheckerGrpc.AutumnCheckerImplBase {
         date = date.substring(0, date.length() - 4);
 
         String sign = "";
-        if (date.compareTo(zodiac[0][1]) < 0)
-            sign += zodiac[0][2];
-        if (date.compareTo(zodiac[1][1]) < 0)
-            sign += zodiac[1][2];
-        if (date.compareTo(zodiac[2][1]) < 0)
-            sign += zodiac[2][2];
+        if (date.compareTo(zodiac[2][1]) <= 0)
+            sign = zodiac[2][2];
+        if (date.compareTo(zodiac[1][1]) <= 0)
+            sign = zodiac[1][2];
+        if (date.compareTo(zodiac[0][1]) <= 0)
+            sign = zodiac[0][2];
 
         ZodiacChecker.ZodiacReply reply = ZodiacChecker.ZodiacReply.newBuilder().setResult("Client with birth date " + request.getDate() + " is a " + sign).build();
+        responseObserver.onNext(reply);
+        responseObserver.onCompleted();
     }
 
 }
